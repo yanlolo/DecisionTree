1bffb6e0852573b9de082633f3aa4253d3e8e636
yanlolo <yanlolo2012@gmail.com>
2015-03-16 16:47:21 +0100

    regression

--- src/regression.scala
+++ src/regression.scala
@@ -1,40 +1,32 @@
-// https://spark.apache.org/docs/1.2.0/mllib-ensembles.html
-// The example below demonstrates how to load a LIBSVM data file
-// parse it as an RDD of LabeledPoint and then perform regression using a Random Forest.
+// source: https://spark.apache.org/docs/1.2.0/mllib-decision-tree.html
+// decision tree with variance as an impurity measure and a maximum tree depth of 4
 // The Mean Squared Error (MSE) is computed at the end to evaluate goodness of fit.
 
-// Test on Linux VM REPL
 
-
-import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.DecisionTree
 import org.apache.spark.mllib.util.MLUtils
 
 // Load and parse the data file.
-val data = MLUtils.loadLibSVMFile(sc, "/home/yyan/Desktop/data/sample_libsvm_data.txt")
+val data = MLUtils.loadLibSVMFile(sc, "/home/yyan/Desktop/data/housing.txt")
 // Split the data into training and test sets (30% held out for testing)
 val splits = data.randomSplit(Array(0.7, 0.3))
 val (trainingData, testData) = (splits(0), splits(1))
 
-// Train a RandomForest model.
+// Train a DecisionTree model.
 //  Empty categoricalFeaturesInfo indicates all features are continuous.
-val numClasses = 2
 val categoricalFeaturesInfo = Map[Int, Int]()
-val numTrees = 3 // Use more in practice.
-val featureSubsetStrategy = "auto" // Let the algorithm choose.
 val impurity = "variance"
 val maxDepth = 4
 val maxBins = 32
 
-val model = RandomForest.trainRegressor(trainingData, categoricalFeaturesInfo,
-  numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+val model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo, impurity,
+  maxDepth, maxBins)
 
 // Evaluate model on test instances and compute test error
 val labelsAndPredictions = testData.map { point =>
   val prediction = model.predict(point.features)
   (point.label, prediction)
 }
-
-//labelsAndPredictions ==> the predictions 
-val testMSE = labelsAndPredictions.map{ case(v, p) => math.pow((v - p), 2)}.mean() // caculate MSE
+val testMSE = labelsAndPredictions.map{ case(v, p) => math.pow((v - p), 2)}.mean()
 println("Test Mean Squared Error = " + testMSE)
-println("Learned regression forest model:\n" + model.toDebugString)
+println("Learned regression tree model:\n" + model.toDebugString)
